3ba37f5cc9e1dd523ca2b4d3f4d0bd1700de69a0
Saeed <saeed.ap98@gmail.com>
2024-09-18 19:52:34 +0330

    update2

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
+import cartReducer from './CreatSlice';
 
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
-export default store
+
+export default store;
